3738ce324eb8f5886a2fe3108ec718051da69eda
Sourav Saha <souravsaha1605@gmail.com>
2017-04-10 18:25:16 +0530

    created inverse cache function

--- cachematrix.R
+++ cachematrix.R
@@ -41,17 +41,3 @@ cacheSolve <- function(x, ...) {
 }
 
 
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
